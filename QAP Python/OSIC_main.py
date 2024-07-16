b76e71075b33a7792657cd2d50b6d6f076bd9be9
Colin Yetman <colinguitarist@gmail.com>
2024-07-16 12:48:45 -0230

    file setups

---
+++ QAP Python/OSIC_main.py
@@ -0,0 +1,27 @@
+# description: One Stop Insurance Company policy calulator
+# author: Colin G. Yetman
+# date(s): 2024-07-16 - 
+
+# import libraries
+
+
+# pull constants from dat file
+f = open("const.dat", "r")
+f.close()
+
+
+# define functions 
+
+
+
+while True:
+    # main program loop
+
+
+
+
+
+
+    break
+
+# house keeping at the end of the program
